78db92c24c646225193266b04fd2d416b28a7dba
Anderson Paiva <anderson.adolfo1998@gmail.com>
2026-05-31 11:20:07 -0300

    feat: finalizando tela de cadastro

--- src/screens/Register/components/RegisterForm.tsx
+++ src/screens/Register/components/RegisterForm.tsx
@@ -1,26 +1,36 @@
-import { useForm } from "react-hook-form";
-import { AppInput } from "../../../components/AppInput";
-import { AppButton } from "../../../components/AppButton";
-import { Text, View } from "react-native";
+import { Text, View } from "react-native"
+import { AppInput } from "../../../components/AppInput"
+import { AppButton } from "../../../components/AppButton"
+import { useForm } from "react-hook-form"
 import { useNavigation, NavigationProp } from "@react-navigation/native";
 import { PublicStackRoutes } from "../../../routes/publicRoutes";
 
-export interface FormLoginParams {
+export interface FormRegisterParams {
     email: string;
+    name: string;
     password: string;
+    consfirm_password: string;
 }
 
-export const LoginForm = () => {
+export const RegisterForm = () => {
     const {
         control,
         handleSubmit,
         formState : { isSubmitting }
-    } = useForm<FormLoginParams>()
+    } = useForm<FormRegisterParams>()
 
     const navigation = useNavigation<NavigationProp<PublicStackRoutes>>();
 
     return (
         <>
+            <AppInput
+                control={control}
+                name="name"
+                label="NOME"
+                leftIconName="person"
+                placeholder="Seu nome"
+            />
+
             <AppInput
                 control={control}
                 name="email"
@@ -38,17 +48,26 @@ export const LoginForm = () => {
                 secureTextEntry
             />
 
+            <AppInput
+                control={control}
+                name="consfirm_password"
+                label="CONFIRMAR SENHA"
+                leftIconName="lock-outline"
+                placeholder="Confirme sua senha"
+                secureTextEntry
+            />
+
             <View className="flex-1 justify-between mt-8 mb-6 min-h-[250px]">
                 <AppButton iconName="arrow-forward">
-                    Login
+                    Cadastrar
                 </AppButton>
 
                 <View>
                     <Text className="mb-6 text-gray-300 text-base">
-                        Ainda não possui uma conta?
+                        Já possui uma conta?
                     </Text>
-                    <AppButton iconName="arrow-forward" mode="outline" onPress={() => navigation.navigate("register")}>
-                        Cadastrar
+                    <AppButton iconName="arrow-forward" mode="outline" onPress={() => navigation.navigate("login")}>
+                        Acessar
                     </AppButton>
                 </View>
             </View>
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,9 +1,15 @@
-import { Text, View } from "react-native"
+import { DismissKeyboardView } from "../../components/DismissKeyboardView"
+import { AuthHeader } from "../../components/AuthHeader"
+import { View } from "react-native"
+import { RegisterForm } from "./components/RegisterForm"
 
 export const Register = () => {
     return (
-        <View>
-            <Text>Tela de registro</Text>
-        </View>
+        <DismissKeyboardView>
+            <View className="flex-1 w-[82%] self-center">
+                <AuthHeader />
+                <RegisterForm />
+            </View>
+        </DismissKeyboardView>
     )
 }
